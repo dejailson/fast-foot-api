d8414cb0e8782703e1e1926d41acd30561a92afa
dejailson <dejailson@192.168.68.100>
2020-05-18 17:13:27 -0300

    Projeto Fast Food Api

---
+++ FastFoodApi/src/main/java/br/ifma/ita/fastfood/FastFoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.ifma.ita.fastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodApiApplication.class, args);
+	}
+
+}
---
+++ FastFoodApi/src/test/java/br/ifma/ita/fastfood/FastFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifma.ita.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
